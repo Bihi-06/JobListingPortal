7ac2ab6d39e51244b67d6dd326a915a1f5db8091
Bihi-06 <aitsaid.ayoub@etu.uae.ac.ma>
2024-10-10 17:23:28 +0100

    test commit

---
+++ JobListingPortal/src/main/java/com/example/joblistingportal/JobListingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.joblistingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingPortalApplication.class, args);
+	}
+
+}
